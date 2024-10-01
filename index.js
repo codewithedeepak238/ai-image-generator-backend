2c472ca477b571e496c0b6bac4b3a1ce8e212581
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-10-01 07:23:06 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import * as dotenv from 'dotenv';
-import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import router from "./routes/dalleRoute.js";
 import postrouter from "./routes/postRoute.js";
@@ -10,7 +9,6 @@ dotenv.config();
 
 //Middlewares
 const app = express();
-app.use(cors());
 app.use(express.json({ limit: '50mb'}));
 
 
