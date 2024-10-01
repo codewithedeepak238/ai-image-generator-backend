89dd07dc82da486272326e5eea55eee7d5553fa7
Deepak Saraswat <deepaksaraswat238@gmail.com>
2024-10-01 07:29:34 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import * as dotenv from 'dotenv';
+import cors from "cors";
 import connectDB from "./mongodb/connect.js";
 import router from "./routes/dalleRoute.js";
 import postrouter from "./routes/postRoute.js";
+
+const PORT = process.env.PORT || 8080;
 
 dotenv.config();
 
 
 //Middlewares
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: '50mb'}));
 
 
@@ -19,7 +23,7 @@ app.use('/api/all-posts', postrouter);
 const startServer = async ()=>{
     connectDB(process.env.MONGODB_URL);
     try{
-        app.listen(8080, ()=>console.log("Server Started!!"));
+        app.listen(PORT, ()=>console.log("Server Started!!"));
     }catch(err){
         console.log(err);
     }
